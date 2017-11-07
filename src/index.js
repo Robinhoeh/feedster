11980c691b0af8c1200818d552b4b29fbd9bb040
Robin Watson <drumsrobin@gmail.com>
2017-11-07 15:54:51 -0500

    comments

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-import $ from 'jquery';
+import $ from 'jquery';//importing jQuery - npm install jQuery and boilerplate
 
-$(document).ready(() => {
-	$('.menu').on('mouseenter', () => {
-		$('.nav-menu').show();
+$(document).ready(() => {//wrap code in doc ready
+	$('.menu').on('mouseenter', () => {//when mouse hovers over .menu
+		$('.nav-menu').show();//display .nav-menu
 	})
-	$('.nav-menu').on('mouseleave', () => {
-		$('.nav-menu').hide();
+	$('.nav-menu').on('mouseleave', () => {//when mouse leaves
+		$('.nav-menu').hide();//hide the .nav-menu
 	});
 	
-	$('.btn').on('mouseenter', (event) => {
-		$(event.currentTarget).addClass('btn-hover');
-	}).on('mouseleave', (event) => {
-		$(event.currentTarget).removeClass('btn-hover');
+	$('.btn').on('mouseenter', (event) => {//when mouse hovers btn
+		$(event.currentTarget).addClass('btn-hover');//ONLY element being hovered add btn-hover
+	}).on('mouseleave', (event) => {//mouse leaves
+		$(event.currentTarget).removeClass('btn-hover');//only on hovered element remove class
 	})
 
-	$('.postText').on('keyup', (event) => {
-		$(event.currentTarget).focus();
+	$('.postText').on('keyup', (event) => {//when a key is pushed down then up
+		$(event.currentTarget).focus();//.focus expects an event
 		const post = $(event.currentTarget).val();//GETS users input from .postText
-		let remaining = 140 - post.length;
+		let remaining = 140 - post.length;// remaining variable SETS and holds value
 		$('.characters').html(remaining);//.html would work here too
 
 		// if (post.length >= 140) {
